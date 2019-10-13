274dafe48663e63c4c114ab11a8d66920bf94486
Hossam Hamada <hossam.fawzy96@gmail.com>
2019-10-13 16:45:48 +0200

    ready to push

---
+++ Virtual Tourist/View/LocationCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  LocationCollectionViewCell.swift
+//  Virtual Tourist
+//
+//  Created by Hossameldien Hamada on 10/11/19.
+//  Copyright © 2019 Hossameldien Hamada. All rights reserved.
+//
+
+import UIKit
+
+class LocationCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Outlets
+    @IBOutlet weak var locationImage: UIImageView!
+    
+}
